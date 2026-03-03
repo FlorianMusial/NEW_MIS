c896b69ba525b70b576e77c683213333bd726f2f
FlorianMusial <musialflorian1999@gmail.com>
2026-03-03 11:03:42 +0100

    New component added. Firebase API keys?

---
+++ AUXILIARY_OBJECTS/ProegsDB.js
@@ -0,0 +1,20 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const ProEgFirebaseConfig = {
+  apiKey: "AIzaSyAwROrWuEbtxFGIoHU5ABsseqz8vWeMFVU",
+  authDomain: "proegs-entries.firebaseapp.com",
+  projectId: "proegs-entries",
+  storageBucket: "proegs-entries.firebasestorage.app",
+  messagingSenderId: "465814528430",
+  appId: "1:465814528430:web:6de3c025700493214be83f",
+  measurementId: "G-W4TTZVMRYJ"
+};
+
+const ProEgsApp = getApps().find(app => app.name === "ProEgConf")
+  ? getApp("ProEgConf")
+  : initializeApp(ProEgFirebaseConfig, "ProEgConf");
+
+const ProEgsDb = getFirestore(ProEgsApp);
+
+export { ProEgsApp, ProEgsDb }
